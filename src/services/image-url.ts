fd3ec2d54c88a9092ffd6e5b5d6fd347de2461dd
Ruchi Agrawal <ruchi.ggc@gmail.com>
2024-05-18 18:47:14 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (url === null) return "";
+  if (url === null) return noImage;
   const target = "media/";
   const mediaIdx = url.indexOf(target) + target.length;
 
